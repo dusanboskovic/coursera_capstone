148a12d746b20ddfaac45406f862c6734c35df49
Dusan-Boskovic <dusan.boskovic@ibm.com>
2020-03-21 23:37:59 -0400

    completed dictionary cleanup and dataframe parsing

--- SegmentingClusteringNeighborhoodsToronto.py
+++ SegmentingClusteringNeighborhoodsToronto.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd 
 import numpy as np 
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,21 +13,43 @@ html = requests.get(url_to_open)
 soup = BeautifulSoup(html.content, 'lxml')
 table = soup.find("table").find("tbody").find_all("tr")
 
-postalCodes = {}
+postalCodes_list = []
+borough_list = []
+neighbourhood_list = []
+
+# chars to remove
+bad_chars = '()'
 i = 0
 for row in table:
     rows = row.find_all('td')
     for x in rows:
-        key = x.get_text().strip()[:3]
-        value = x.get_text().strip()[3:]
-        postalCodes[key] = value
+        x_clean = x.get_text().strip()
+        postalCodes_list.append(x_clean[:3])
+        borough = (x_clean[3:]).split('(')[0]
+        borough_list.append(borough)
+        match = re.search(r"\([^)]+\)", x_clean)
+        if match:
+            match_text = match.group(0)
+            for c in bad_chars: match_text = match_text.replace(c, "")
+            match_text = match_text.replace('/', ",")
+            neighbourhood_list.append(match_text)
+        else:
+            neighbourhood_list.append(borough.replace('/', ","))
 
-column_names = ['Postal Code', 'Borough', 'Neighbourhood']
-df  = pd.DataFrame.from_dict(data=postalCodes, orient='index')
-df.head()
-    
+postalCodes = {"Postal Code" : postalCodes_list}
+boroughs = {"Boroughs" : borough_list}
+neighbourhoods = {"Neighbourhoods" : neighbourhood_list}
+
+# %%
+dict_list = [postalCodes, boroughs, neighbourhoods]
+df1  = pd.DataFrame.from_dict(postalCodes)
+df2 = pd.DataFrame.from_dict(boroughs)
+df3 = pd.DataFrame.from_dict(neighbourhoods)
+df = pd.concat([df1, df2, df3], axis=1, sort=False)
+df = df[~df.Boroughs.str.contains('Not assigned')].reset_index(drop=True)
 
 
-
+# %%
+df.shape
 
 # %%
